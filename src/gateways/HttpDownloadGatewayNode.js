a237921f49db4b8f2845b904b112d03622d2db42
Israel Fonseca <israel.bgf@gmail.com>
2016-10-22 11:22:22 -0200

    extracted httpdownloadgateway

---
+++ src/gateways/HttpDownloadGatewayNode.js
@@ -0,0 +1,22 @@
+var http = require('http')
+var fs = require('fs')
+var path = require('path');
+
+class HttpDownloadGateway {
+
+    download(url, destinationFolder) {
+        return new Promise((resolve, reject) => {
+            let fileName = path.basename(url)
+            var downloadedFileLocation = path.join(destinationFolder, fileName);
+            var file = fs.createWriteStream(downloadedFileLocation)
+            http.get(url, (response) => {
+                response.pipe(file).on('finish', ()=> {
+                    resolve(downloadedFileLocation)
+                })
+            })
+        })
+    }
+
+}
+
+module.exports = HttpDownloadGateway
---
+++ src/gateways/HttpDownloadGatewayNodeTest.js
@@ -0,0 +1,23 @@
+var it = require('mocha').it
+var describe = require('mocha').describe
+var HttpDownloadGatewayNode = require('./HttpDownloadGatewayNode')
+var fs = require('fs');
+var path = require('path');
+var assert = require('chai').assert
+
+describe('HttpDownloadGatewayNode', function () {
+
+    it('should return download file from internet', function () {
+        let fileToDownload = 'robots.txt'
+        let dirToReceiveDownload = 'test/output'
+
+        return new HttpDownloadGatewayNode().download(`http://www.google.com/${fileToDownload}`, dirToReceiveDownload)
+            .then((downloadedFileLocation) => {
+                let expectedFileLocation = path.join(dirToReceiveDownload, fileToDownload)
+                assert.equal(downloadedFileLocation, expectedFileLocation)
+                let content = fs.readFileSync(downloadedFileLocation, 'utf8')
+                assert.include(content, 'User-agent:')
+            })
+    })
+})
+
